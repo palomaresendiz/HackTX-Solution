70b956d1e005361a0646dd188c71ecc4641086ad
b.schlueter <schlueter.braden@gmail.com>
2023-10-22 00:15:12 -0500

    modified userSignUp.js. Still isnt working

--- js/userSignUp.js
+++ js/userSignUp.js
@@ -1,5 +1,5 @@
 // Initialize Parse
-Parse.initialize("vYOu7vgqNnE9f4j7gKqppBNcWAprWDGBVC3SV8u8");
+Parse.initialize("vYOu7vgqNnE9f4j7gKqppBNcWAprWDGBVC3SV8u8, rnIdMgPC2xXcrMDzZ1WmoHVSyUy999wyQdjkEWNQ");
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 // Get references to input elements
@@ -7,30 +7,23 @@ var nwUsernameInput = document.getElementById("nwUsername");
 var nwPasswordInput = document.getElementById("nwPassword");
 var repeatPasswordInput = document.getElementById("repeatPassword");
 
-// Attach an event listener to the form's submit button
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+signUp();
 
-    // Get the values from the input fields
-    var username = nwUsernameInput.value;
-    var password = nwPasswordInput.value;
-    var repeatPassword = repeatPasswordInput.value;
-
-    if (password !== repeatPassword) {
-        alert("Passwords do not match. Please try again.");
-    } else {
-        signUp(username, password);
-    }
-});
-
-function signUp(username, password) {
+function signUp() {
+    // Create a new instance of the user class
     var user = new Parse.User();
-    user.set("username", username);
-    user.set("password", password);
-
-    user.signUp().then(function (user) {
-        console.log('User created successfully with name: ' + user.get("username"));
-    }).catch(function (error) {
+    user.set("username", nwUsernameInput);
+    user.set("password", nwPasswordInput);
+    user.set("repeatPassword", repeatPasswordInput);
+  
+    if (nwPasswordInput != repeatPasswordInput) {
+        alert("Passwords do not match");
+    }
+    
+  
+    user.signUp().then(function(user) {
+        console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+    }).catch(function(error){
         console.log("Error: " + error.code + " " + error.message);
     });
 }
